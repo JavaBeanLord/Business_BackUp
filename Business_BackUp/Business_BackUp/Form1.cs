7054c6b15f5fa1e517e34199846c35c9940e5299
Justin <justinrgarr@zonecomputers.org>
2014-04-08 22:46:11 -0400

    New...LoginFOrm.

---
+++ Business_BackUp/Business_BackUp/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Business_BackUp{
+
+    public partial class Form1 : Form 
+    {
+        private const int WM_NCHITTEST = 0x84;
+        private const int HTCLIENT = 0x1;
+        private const int HTCAPTION = 0x2;
+
+        protected override void WndProc(ref Message message)
+        {
+            base.WndProc(ref message);
+
+            if (message.Msg == WM_NCHITTEST && (int)message.Result == HTCLIENT)
+                message.Result = (IntPtr)HTCAPTION;
+        }
+         
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text == ("password"))
+            {
+                MessageBox.Show("Yes");
+                textBox2.Text = ("");
+            }
+            else 
+            {
+                MessageBox.Show("Password Could be Wrong or Null!");
+                textBox2.Text = ("");
+            }
+        }
+ 
+    }
+}
